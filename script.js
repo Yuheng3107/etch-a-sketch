2c7772091fa4ca562276b8202ea16ede3a1edaf5
Kuang Yu Heng <yuheng3107@gmail.com>
2022-10-07 23:07:28 +0800

    Completed Simple Version of Project

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
-const grid = document.querySelector('#grid');
+
 // constructing the grid
 
 function generateGrid(n) {
+    const grid = document.createElement('div');
+    grid.setAttribute('id', 'grid');
+    const container = document.querySelector('.container');
+    container.appendChild(grid);
     for (let i = 0; i < n; i++) {
         let row = document.createElement('div');
         row.style.display = 'flex';
@@ -15,18 +19,32 @@ function generateGrid(n) {
         }
         grid.appendChild(row);
     }
+    // adding event listeners to the grid
+
+
+    const squares = document.querySelectorAll('.square');
+
+    // add color change when there is mouseenter event
+    squares.forEach(square => square.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'blue';
+    } )); 
 }
 generateGrid(16);
 
-// adding event listeners to the grid
 
 
-const squares = document.querySelectorAll('.square');
-
-// add color change when there is mouseenter event
-squares.forEach(square => square.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = 'blue';
-} )); 
-
+const btn = document.querySelector('button');
+console.log(btn);
+btn.addEventListener('click', (e) => {
+    let n = -1;
+    while (n < 0 || n > 100) {
+        n = prompt("Number of squares per side for the new grid: ")
+    }
+    
+    let grid = document.querySelector('#grid');
+    const container = document.querySelector('.container');
+    container.removeChild(grid);
+    generateGrid(n);
+});
 
 
